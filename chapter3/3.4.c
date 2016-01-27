56d8e5a71a55e6aedac45dfb869f513e0e6a679a
rzhengyang <billy.ruzheng@gmail.com>
2016-01-27 16:55:39 +0800

    question

---
+++ chapter3/3.4.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	int n,num,b,i,j;
+	char s[100],s1[100],s2[100];
+	scanf("%d",&n);
+	while(n--)
+	{
+ 		getchar();  
+        	gets(s);
+		num= strlen(s);
+		for(i=1;i<=num;i++)
+		{
+			if(s[i] == " ")
+			{
+				printf("%d\n",i);
+				printf("\n");
+				break;
+			}
+			if(num%i==0)
+			{
+				b=num/i;
+				memset(s2,0,100*sizeof(char));
+				memset(s1,0,100*sizeof(char));
+				strncpy(s1,s,i*sizeof(char));
+				for(j=1;j<=b;j++)
+				{	
+					strcat(s2,s1);
+				}
+				if(strcmp(s,s2)==0)
+				{
+					printf("%d\n",i);
+					printf("\n");
+					break;
+				}
+			}
+		}
+	}
+	return 0;
+}
