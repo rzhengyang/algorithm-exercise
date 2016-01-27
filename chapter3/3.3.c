b4fb4efbdbfe3affaacc84b073e1eb596aa4b247
rzhengyang <billy.ruzheng@gmail.com>
2016-01-27 10:45:44 +0800

    Accepted

---
+++ chapter3/3.3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	int num[10];
+	int i,n,x,j,a;
+	scanf("%d",&n);
+	while(n--)
+	{
+		memset(num,0,sizeof(num));
+		scanf("%d",&x);
+		for(i=1;i<=x;i++)		
+		{
+			a=i;
+			while(a)
+			{	
+				
+				num[a%10]++;
+				a=a/10;
+			}
+		}
+		for(j=0;j<9;j++)
+		{
+			printf("%d ",num[j]);
+		}
+		printf("%d\n",num[9]);
+	}
+
+
+}
