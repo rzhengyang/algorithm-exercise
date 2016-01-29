13ee7fc91dc002f3f1d0d6c09659c7ad4da183f4
rzhengyang <billy.ruzheng@gmail.com>
2016-01-29 13:25:36 +0800

    Accepted

--- chapter3/3.4.c
+++ chapter3/3.4.c
@@ -1,35 +1,34 @@
-#include<stdio.h>
-#include<string.h>
-int main(void)
+#include <stdio.h>
+#include <string.h>
+
+int main()
 {
-	int n,num,b,i,j;
-	char s[100],s1[100],s2[100];
-	scanf("%d",&n);
-	while(n--)
-	{
- 		getchar();  
-        	gets(s);
-		num= strlen(s);
-		for(i=1;i<=num;i++)
-		{
-			if(num%i==0)
-			{
-				b=num/i;
-				memset(s2,0,100*sizeof(char));
-				memset(s1,0,100*sizeof(char));
-				strncpy(s1,s,i*sizeof(char));
-				for(j=1;j<=b;j++)
-				{	
-					strcat(s2,s1);
-				}
-				if(strcmp(s,s2)==0)
-				{
-					printf("%d\n",i);
-					printf("\n");
-					break;
-				}
-			}
-		}
-	}
-	return 0;
+    char s[85];
+    int n;
+    scanf("%d", &n);
+
+    while(n--)
+    {
+        scanf("%s", s);
+        int len = strlen(s);
+        int k, i;
+        for(i = 1; i <= len; ++i)
+        {
+            if(len % i == 0)
+            {
+                for(k = i; k < len; ++k)
+                {
+                    if(s[k] != s[k % i])
+                        break;
+                }
+                if(k == len)
+                {
+                    printf("%d\n", i);
+                    break;
+                }
+            }
+        }
+        if(n) printf("\n");
+    }
+    return 0;
 }
